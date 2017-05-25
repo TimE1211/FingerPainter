b57ec7beaaab12969cbcae5e7d4ca53b9d2d3a13
TimE1211 <timmy121192@gmail.com>
2017-05-25 11:48:57 -0400

    same as commit before

--- FingerPainter/Project.swift
+++ FingerPainter/Project.swift
@@ -91,7 +91,6 @@ class Line
     ]
   }
   
-  
   init(json: JSON)
   {
     start = CGPoint()
@@ -101,6 +100,43 @@ class Line
     start.y = json["starty"].cgFloatValue
     end.x = json["endx"].cgFloatValue
     end.y = json["endy"].cgFloatValue
+  }
+}
+
+class User
+{
+  var userId: String
+  var username: String
+  var password: String
+  
+  init()
+  {
+    self.userId = ""
+    self.username = ""
+    self.password = ""
+  }
+  
+  init(userId: String, username: String, password: String)
+  {
+    self.userId = userId
+    self.username = username
+    self.password = password
+  }
+  
+  init(json: JSON)
+  {
+    userId = json["userId"].stringValue
+    username = json["username"].stringValue
+    password = json["password"].stringValue
+  }
+  
+  func postBody() -> [String: Any]
+  {
+    return [
+      "userId": userId,
+      "username": username,
+      "password": password,
+    ]
   }
 }
 
